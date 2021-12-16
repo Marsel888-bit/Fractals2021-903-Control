d701ae1ad3b3aeb72e43975c6d98f0a3943f8fea
Марсель <marsel-garifullin888@mail.ru>
2021-12-17 00:19:11 +0300

    Реализация метода создания видео

--- src/main/kotlin/ru/smak/ui/painting/Video.kt
+++ src/main/kotlin/ru/smak/ui/painting/Video.kt
@@ -1,25 +1,22 @@
 package ru.smak.ui.painting
 
-import ru.smak.ui.GraphicsPanel
-import java.awt.event.MouseAdapter
-import java.awt.event.MouseEvent
+import java.io.humble.video.*
+import java.io.humble.video.awt.MediaPictureConverter
+import java.io.humble.video.awt.MediaPictureConverterFactory
 import java.awt.image.BufferedImage
-import javax.swing.Box
-
-
 
 class Video {
     private val buffImgList: MutableList<BufferedImage>
+    private val duration: Int // длительность видео
+    private val fps: Int // количество кадров в секунду
+
 
     init {
         buffImgList = mutableListOf<BufferedImage>().apply {
 
-
-
-
-
-
         }
+        duration = 120
+        fps = 15
     }
 
     fun addBuffImg(img: BufferedImage) {
@@ -35,16 +32,54 @@ class Video {
         //buffImgList.remove(img)
     }
 
+    fun createVideo(fileName: String, formatName: String, duration: Int, fps: Int) {
+        val framerate = 1 / fps // кадровая частота
 
+        val muxer = Muxer.make(fileName, null, formatName) // контейнер для файла
+        val format = muxer.format
+        val codec: Codec = Codec.findEncodingCodec(format.defaultVideoCodecId) // кодек, подходящий для нашего формата - устройство для преобразования данных
 
+        val encoder = Encoder.make(codec) // создаём энкодер
 
+        encoder.width = 300 // ширина кадров
+        encoder.height = 100 // высота кадров
 
+        val pixelFormat = PixelFormat.Type.PIX_FMT_YUV420P
+        encoder.pixelFormat = pixelFormat
+        encoder.timeBase = framerate
 
+        if (format.getFlag(ContainerFormat.Flag.GLOBAL_HEADER)) encoder.setFlag(Coder.Flag.FLAG_GLOBAL_HEADER, true) // устанавливаем флаги энкодера
+        encoder.open(null, null)
 
+        muxer.addNewStream(encoder)
+        muxer.open(null, null)
 
+        var converter: MediaPictureConverter? = null // конвертер для преобразования изображения к нужному формату
+        val picture = MediaPicture.make(
+            encoder.width,
+            encoder.height,
+            pixelFormat
+        )
+        picture.timeBase = framerate
 
+        val packet = MediaPacket.make()
+        var i = 0
+        while (i < duration / framerate.toDouble()) { // кодируем изображения и добавляем в пакет
 
-
-
-
+            buffImgList.forEach {
+                if (converter == null) converter = MediaPictureConverterFactory.createConverter(it, picture)
+                converter!!.toPicture(picture, it, i.toLong())
+                do {
+                    encoder.encode(packet, picture)
+                    if (packet.isComplete) muxer.write(packet, false)
+                } while (packet.isComplete)
+                i++
+            }
+        }
+        do {
+            encoder.encode(packet, null)
+            if (packet.isComplete) muxer.write(packet, false)
+        } while (packet.isComplete)
+        muxer.close()
+    }
 }
